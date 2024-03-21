701ded9c75e1f205f60d6c5f31b9de9e19e8f81b
whodatboi04 <ryanmarkdelosreyes04@gmail.com>
2024-03-21 22:05:13 +0800

    Added About Me / Modify the approach

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -5,12 +5,13 @@ import { MdOutlineMail } from "react-icons/md";
 import { MdDownload } from "react-icons/md";
 
 
-const Home = () => {
+const Portfolio = () => {
   return (
-    <main className='flex flex-col md:flex-row w-3/4 items-center m-auto h-96 mt-10 md:mt-20 '>
-        <section className='flex flex-col gap-10 md:flex-row md:gap-72'>
+    <main className='flex flex-col justify-center items-center'>
+        {/* Home */}
+        <section className='flex flex-col-reverse items-center mt-6 md:flex-row justify-center md:gap-96 md:w-3/4 md:mt-24'>
           <div>
-            <h3 className='text-xs md:text-sm font-bold text-mainColor text-center lg:text-left'>FRONT-END DEVELOPER</h3>
+            <h3 className='text-xs md:text-sm font-bold text-mainColor text-center lg:text-left mt-8'>FRONT-END DEVELOPER</h3>
             <h1 className='text-4xl md:text-6xl font-bold text-center lg:text-left'>Hello, my name <br/> is <span className='text-mainColor'>Ryan Mark</span></h1>
             <p className='text-sm md:text-lg text-center lg:text-left text-lightColor mt-2'>an aspiring developer with passion for <br/> design,innovation, technologies
             </p>
@@ -21,20 +22,37 @@ const Home = () => {
           </div>
           <div>
             <div className='bg-gradient-to-b from-mainColor to-lightGreen rounded-full w-72 md:w-80'>
-              <div></div>
               <img src='../src/image/1-profile-img.png' className='w-72 md:ml-6 md:mb-6 md:w-80'/>
             </div>
           </div>
+        </section>
+        {/* ABOUT ME */}
+        <section className=''>
+          <img 
+            src='../src/image/my-img-front.png' 
+            className='rounded-full mt-10 w-72 m-auto'
+          />
+          <h1>About Me</h1>
+          <p>
+            A self-motivated and enthusiastic individual IT graduate from <br />
+            Universidad de Manila, equipped with a diverse skill set in <br />
+            communication, collaboration, and analytical thinking. Committed to <br />
+            continuous learning, I thrive on exploring new horizons for personal <br />
+            and professional growth.
+          </p>
+          <h1>Skills</h1>
+
         </section>
     </main>
   )
 }
 
+
 function App() {
   return (
     <Router>
       <Navbar />
-      <Home />
+      <Portfolio />
     </Router>
   )
 }
